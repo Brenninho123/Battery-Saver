f5a5d81e593a3edfcd66c0952a550d4bb6378c8a
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-09-17 18:49:24 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'battery-saver-v1';
+const CACHE_NAME = 'battery-saver-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -45,6 +45,25 @@ self.addEventListener('fetch', (event) => {
           return caches.match('./index.html');
         }
       });
+    })
+  );
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+      if (clientList.length > 0) {
+        let client = clientList[0];
+        for (let i = 0; i < clientList.length; i++) {
+          if (clientList[i].focused) {
+            client = clientList[i];
+            break;
+          }
+        }
+        return client.focus();
+      }
+      return clients.openWindow('./index.html');
     })
   );
 });
